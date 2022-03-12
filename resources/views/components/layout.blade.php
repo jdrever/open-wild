ea309f3bf643c23f0c036f2bd75a92c4ab53a837
James Drever <james@careful.digital>
2022-03-12 16:23:48 +0000

    initial commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta content="Promoting the enjoyment, understanding and conservation of the flora of Shropshire" name="description" />
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<!-- Custom styles for this template -->
+	<link href="/css/app.css" rel="stylesheet">
+	<link href="/css/enhancements.css" rel="stylesheet">
+	<!-- A2HS -->
+	<link rel="manifest" href="/manifest.webmanifest">
+	<!-- Mapping -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css" integrity="sha256-BPfK9M5v34c2XP6p0cxVz1mUQLst0gTLk0mlc7kuodA=" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet-src.js" integrity="sha256-dG7rUAec0UN0e3nlMEuYJzsbK0rpoz4hBbNp0ir9iF8=" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/wicket@1.3.6/wicket.min.js"></script>
+	<script type="text/javascript" src="/js/proj4.js"></script>
+	<script type="text/javascript" src="/js/Leaflet.MetricGrid.js"></script>
+	<script type="text/javascript" src="/js/leaflet.wms.js"></script>
+	<script type="text/javascript" src="/js/BasicMap.js"></script>
+	<script>
+		if (navigator && navigator.serviceWorker)
+		{
+  			navigator.serviceWorker.register('/js/sw.js');
+		}
+	</script>
+	<title>
+    {{ $title ?? 'Shrop Bot' }}
+	</title>
+</head>
+
+<body>
+	<div class="container-fluid p-2 pt-0 mt-2">
+		<nav class="navbar navbar-expand-sm navbar-light px-3 py-2">
+			<a class="navbar-brand fs-4" href="/">Botanical Records</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="navbar-collapse collapse flex-grow-1 text-right text-white" id="navbar-main">
+				<ul class="nav navbar-nav me-auto flex-nowrap">
+					<li class="nav-item">
+						<a class="nav-link align-baseline" href="/species">Species</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/sites">Sites</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/squares">Squares</a>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav me-0">
+					<li class="nav-item">
+						<a class="nav-link" href="/about">About</a>
+					</li>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<?php if (isset($error)) : ?>
+			<div class="alert alert-warning alert-dismissible fade show" role="alert">
+				<?= $error ?>
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+		<?php endif ?>
+		<div class="container-fluid content-inner p-3">
+        {{ $slot }}
+		</div>
+		<footer class="page-footer footer-fluid">
+			<div class="mx-auto mt-2 text-center">
+				<span class="small">Shropshire Botanical Society Data supported by
+					<a href="https://registry.nbnatlas.org/public/show/dp120" target="_blank">National Biodiversity Network</a>
+				</span>
+		<?php if (isset($queryUrl)) : ?>
+				<details style="font-size:small;"><summary>NBN API Query</summary><?= $queryUrl ?></details>
+		<?php endif ?>
+			</div>
+		</footer>
+		<!-- Bootstrap 5-beta2 bundle -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	</div>
+</body>
+</html>
---
+++ resources/views/species-search.blade.php
@@ -0,0 +1,122 @@
+<x-layout>
+
+<h2 class="text-start text-md-center">Search for a Species in Shropshire</h2>
+
+{{-- form_open('species') --}}
+
+<div class="row mb-2">
+	<div class="col-lg-8 mx-auto">
+		<label for="search" class="form-label visually-hidden">Species name</label>
+		<div class="input-group">
+			<input type="text" id="search" class="form-control" name="search" aria-describedby="search-help" placeholder="Species name" value="{{-- set_value('search', $nameSearchString); --}}" />
+			<button type="submit" class="btn btn-primary">List Species</button>
+		</div>
+		<small id="search-help" class="form-text text-start text-md-center d-block">Enter all or part of a species name. Try something like "Hedera".</small>
+	</div>
+</div>
+<div class="row justify-content-center gy-3">
+	<div class="form-group col-sm-4 col-lg-3">
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="name-type" id="scientific-name" value="scientific" onchange="if (this.form.search.value!='') { this.form.submit(); }" {{-- set_radio('name-type', 'scientific', ($nameType === 'scientific')); --}} />
+			<label class="form-check-label" for="scientific-name">
+				scientific<span class="d-none d-lg-inline"> name only</span>
+			</label>
+		</div>
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="name-type" id="common-name" value="common"  onchange="if (this.form.search.value!='') { this.form.submit(); }" {{-- set_radio('name-type', 'common', ($nameType === 'common')); --}} />
+			<label class="form-check-label" for="common-name">
+				common<span class="d-none d-lg-inline"> name only</span>
+			</label>
+		</div>
+		<div class="form-check">
+			<input class="form-check-input" type="checkbox" name="axiophyte-filter" id="axiophyte-filter" value="true"  onchange="if (this.form.search.value!='') { this.form.submit(); }" {{-- set_radio('axiophyte-filter', 'true', ($axiophyteFilter === 'true')); --}} />
+			<label class="form-check-label" for="axiophyte-name">
+				<span class="d-lg-none">axiophytes</span>
+				<span class="d-none d-lg-inline">axiophytes only</span>
+			</label>
+		</div>
+	</div>
+	<div class="form-group col-sm-4 col-lg-3">
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="species-group" id="plants" value="plants"  onchange="if (this.form.search.value!='') { this.form.submit(); }" {{-- set_radio('groups', 'plants', $speciesGroup === 'plants'); --}} />
+			<label class="form-check-label" for="plants">
+				only plants
+			</label>
+		</div>
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="species-group" id="bryophytes" value="bryophytes"  onchange="if (this.form.search.value!='') { this.form.submit(); }"{{--  set_radio('groups', 'bryophytes', $speciesGroup === 'bryophytes'); --}} />
+			<label class="form-check-label" for="bryophytes">
+				only bryophytes
+			</label>
+		</div>
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="species-group" id="both" value="both"  onchange="if (this.form.search.value!='') { this.form.submit(); }" {{-- set_radio('groups', 'both', $speciesGroup === 'both'); --}} />
+			<label class="form-check-label" for="both">
+				both <span class="d-none d-xl-inline">plants and bryophytes</span>
+			</label>
+		</div>
+	</div>
+</div>
+{{-- //form_close() --}}
+
+
+<?php if (isset($message)) { ?>
+	<div class="alert alert-danger" role="alert">
+		I am very sorry, but an error has occured.</b>:  <?= $message ?>
+	</div>
+<?php } ?>
+
+<!-- Show the search results if there are any -->
+<?php if (isset($records)&&count($records)>0) { ?>
+	<?php if (isset($downloadLink)) { ?>
+	<p><a href="<?= $downloadLink ?>">Download this data</a></p>
+	<?php } ?>
+	<table class="table mt-3">
+		<thead>
+			<tr>
+				<th class="d-none d-md-table-cell">Family</th>
+				<th <?php if ($nameType === 'common') { ?>class="d-none d-sm-table-cell" <?php } ?>>Scientific Name</th>
+				<th <?php if ($nameType === 'scientific') { ?>class="d-none d-sm-table-cell" <?php } ?>>Common Name</th>
+				<th>Records</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($records as $species) { ?>
+				<?php $speciesArray = explode('|', (string)$species->label); ?>
+				<tr>
+				<?php if ($nameType === 'scientific') { ?>
+					<td class="d-none d-md-table-cell"><?= $speciesArray[4] ?></td>
+					<td><a href="<?= '/species/' . $speciesArray[0] . '?nameSearchString=' . $nameSearchString ?>"><?= $speciesArray[0] ?></a></td>
+					<td class="d-none d-sm-table-cell">
+						<a href="<?= '/species/' . $speciesArray[0] . '?displayName=' . $speciesArray[2] . '&nameSearchString=' . $nameSearchString ?>"><?= $speciesArray[2] ?></a>
+					</td>
+				<?php } ?>
+				<?php if ($nameType === 'common') { ?>
+					<td class="d-none d-md-table-cell"><?= $speciesArray[5] ?></td>
+					<td class="d-none d-sm-table-cell"><a href="<?= '/species/' . $speciesArray[1] . '?nameSearchString=' . $nameSearchString ?>"><?= $speciesArray[1] ?></a></td>
+					<td>
+						<a href="<?= urldecode('/species/' . $speciesArray[1] . '?displayName=' . $speciesArray[0] . '&nameSearchString=' . $nameSearchString) ?>"><?= $speciesArray[0] ?></a>
+					</td>
+				<?php } ?>
+					<td><?= $species->count ?></td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+{{-- //$this->include('pagination') --}}
+
+	<?php if (isset($downloadLink)) { ?>
+		<p><a href="<?= $downloadLink ?>">Download this data</a></p>
+	<?php } ?>
+<?php }
+else
+{ ?>
+	<?php if (! empty($nameSearchString)) { ?>
+	<div class="alert alert-warning" role="alert">
+		No records could be found matching those criteria.
+	</div>
+	<?php } ?>
+<?php } ?>
+
+
+</x-layout>
