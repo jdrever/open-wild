482ded5756909a55aee8d5ecd9e7aa254e0693df
James Drever <james@careful.digital>
2022-04-10 08:02:36 +0100

    ensure pagination works without js

--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -1,16 +1,15 @@
 <?php
-$lastSlashInUrl= strripos(url()->current(), "/")+1;
-$urlToLastSlash=substr(url()->current(),0, $lastSlashInUrl)
+$urlIfNoJavaScript=str_replace("-update","", url()->current());
 ?>
 <nav>
 		<ul class="pagination justify-content-center">
 	<?php
 	$range = 3;
 	if ($results->currentPage + $range >7) : ?>
-			<li class="page-item"><a class="page-link" onclick="updateDataset(1); }}); return false;" href="{{ url()->current() . '?page=' . '1' }}">First</a></li>
+			<li class="page-item"><a class="page-link" onclick="updateDataset(1); }}); return false;" href="{{ $urlIfNoJavaScript . '?page=' . '1' }}">First</a></li>
 	<?php endif ?>
 	<?php if ($results->currentPage > 1) : ?>
-			<li class="page-item"><a class="page-link" onclick="updateDataset({{ ($results->currentPage - 1) }}); return false;" href="{{ url()->current() . '?page=' . ($results->currentPage - 1) }}">Previous</a></li>
+			<li class="page-item"><a class="page-link" onclick="updateDataset({{ ($results->currentPage - 1) }}); return false;" href="{{ $urlIfNoJavaScript . '?page=' . ($results->currentPage - 1) }}">Previous</a></li>
 	<?php endif ?>
 	<?php
 		for ($x = ($results->currentPage - $range); $x < (($results->currentPage + $range) + 1); $x++)
@@ -27,18 +26,18 @@ $urlToLastSlash=substr(url()->current(),0, $lastSlashInUrl)
 				else
 				{
 					?>
- 			<li class="page-item"><a class="page-link" onclick="updateDataset({{ $x }}); return false;" href="{{ url()->current() . '?page=' . $x }}"><?= $x?></a></li>
+ 			<li class="page-item"><a class="page-link" onclick="updateDataset({{ $x }}); return false;" href="{{ $urlIfNoJavaScript . '?page=' . $x }}"><?= $x?></a></li>
 		<?php
 			}
 		}
 	} ?>
 	<?php
 	if ($results->currentPage<$results->numberOfPages) : ?>
-			<li class="page-item"><a class="page-link" onclick="updateDataset({{ ($results->currentPage + 1) }}); return false;"  href="{{ url()->current() . '?page=' . ($results->currentPage+1) }}">Next</a></li>
+			<li class="page-item"><a class="page-link" onclick="updateDataset({{ ($results->currentPage + 1) }}); return false;"  href="{{ $urlIfNoJavaScript . '?page=' . ($results->currentPage+1) }}">Next</a></li>
 	<?php endif ?>
 	<?php
 	if (($results->currentPage + $range)<$results->numberOfPages) : ?>
-			<li class="page-item"><a class="page-link" onclick="updateDataset({{ ($results->numberOfPages - 1) }}); return false;"  href="{{ url()->current() . '?page=' . ($results->numberOfPages) }} ">Last</a></li>
+			<li class="page-item"><a class="page-link" onclick="updateDataset({{ ($results->numberOfPages - 1) }}); return false;"  href="{{ $urlIfNoJavaScript . '?page=' . ($results->numberOfPages) }} ">Last</a></li>
 	<?php endif ?>
 	</ul>
 	<p class="text-center" style="font-size:small;"><?= $results->numberOfRecords ?> records in <?= $results->numberOfPages ?> pages </p>
