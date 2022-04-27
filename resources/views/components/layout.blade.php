e16f2191f4c089f214c767a1791cc009a6f766b9
James Drever <james@careful.digital>
2022-04-27 15:44:45 +0100

    brand link goes to home page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -57,7 +57,7 @@
 <header>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">WildSearch</a>
+      <a class="navbar-brand" href="/">WildSearch</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
