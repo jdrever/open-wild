233340a59a6e07a5187b7b615ce56b9fd002e107
James Drever <james@careful.digital>
2022-05-26 15:03:52 +0100

    tidying single record page

--- resources/views/single-record.blade.php
+++ resources/views/single-record.blade.php
@@ -2,13 +2,13 @@
 
 <div class="d-flex align-items-center">
 
-		<a id="back-link" href="/species/<?= $results->scientificName ?>?displayName=<?= $displayName ?>" class="header-backArrow">
+		<a id="back-link" href="/species/{{ $results->scientificName }}?displayName={{ $displayName }}" class="header-backArrow">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
 				<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
 			</svg>
 		</a>
 
-	<h2><?= $displayTitle ?></h2>
+	<h2>{{ $displayTitle }}</h2>
 </div>
 	<ul id="tabs" class="nav nav-tabs d-lg-none" role="tablist">
 		<li class="nav-item" role="presentation">
@@ -25,7 +25,7 @@
 				<thead>
 					<tr>
 						<th scope="col">Record Id</th>
-						<th scope="col"><?= $results->recordId ?></th>
+						<th scope="col">{{ $results->recordId }}</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -42,25 +42,25 @@
 					</tr>
 					<tr>
 						<td scope="row">Full Grid Reference</a></td>
-						<td scope="row"><a href="/square/<?= $results->gridReference ?>/species/<?= $results->scientificName ?>"><?= $results->gridReference ?></a></td>
+						<td scope="row"><a href="/square/{{ $results->gridReference }}/species/{{ $results->scientificName }}">{{ $results->gridReference }}</a></td>
 					</tr>
                     <tr>
 						<td scope="row">Scientific Name</td>
-						<td scope="row"><?= $results->scientificName ?></td>
+						<td scope="row">{{ $results->scientificName }}</td>
 					</tr>
 					<tr>
 						<td scope="row">Common Name</td>
 						<td scope="row">
-							<?= $results->commonName ?>
+							{{ $results->commonName }}
 						</td>
 					</tr>
 					<tr>
 						<td scope="row">Recorders</td>
-						<td scope="row"><?= $results->recorders ?></td>
+						<td scope="row">{{ $results->recorders }}</td>
 					</tr>
 					<tr>
 						<td scope="row">Full Date</td>
-						<td scope="row"><?= $results->fullDate ?></td>
+						<td scope="row">{{ $results->fullDate }}</td>
 					</tr>
                     <tr>
                         <th scope="col" colspan="2">Taxonomy
@@ -68,31 +68,31 @@
                     </tr>
 					<tr>
 						<td scope="row">Species</td>
-						<td scope="row"><?= $results->species ?></td>
+						<td scope="row">{{ $results->species }}</td>
 					</tr>
 					<tr>
 						<td scope="row">Genus</td>
-						<td scope="row"><?= $results->genus ?></td>
+						<td scope="row">{{ $results->genus }}</td>
 					</tr>
 					<tr>
 						<td scope="row">Family</td>
-						<td scope="row"><?= $results->family ?></td>
+						<td scope="row">{{ $results->family }}</td>
 					</tr>
 					<tr>
 						<td scope="row">Order</td>
-						<td scope="row"><?= $results->order ?></td>
+						<td scope="row">{{ $results->order }}</td>
 					</tr>
 					<tr>
 						<td scope="row">Class</td>
-						<td scope="row"><?= $results->class ?></td>
+						<td scope="row">{{ $results->class }}</td>
 					</tr>
 					<tr>
 						<td scope="row">Phylum</td>
-						<td scope="row"><?= $results->phylum ?></td>
+						<td scope="row">{{ $results->phylum }}</td>
 					</tr>
 					<tr>
 						<td scope="row">Kingdom</td>
-						<td scope="row"><?= $results->kingdom ?></td>
+						<td scope="row">{{ $results->kingdom }}</td>
 					</tr>
                     <tr>
                         <th scope="col" colspan="2">Observation
@@ -101,25 +101,25 @@
                     <tr>
 						<td scope="row">Basis of Record</td>
 						<td scope="row">
-							<?= $results->basisOfRecord ?>
+							{{ $results->basisOfRecord }}
 						</td>
 					</tr>
                     <tr>
 						<td scope="row">Verification Status</td>
 						<td scope="row">
-							<?= $results->verificationStatus ?>
+							{{ $results->verificationStatus }}
 						</td>
 					</tr>
                     <tr>
 						<td scope="row">Remarks</td>
 						<td scope="row">
-							<?= $results->remarks ?>
+							{{ $results->remarks }}
 						</td>
 					</tr>
                     <tr>
 						<td scope="row">License</td>
 						<td scope="row">
-							<?= $results->license ?>
+							{{ $results->license }}
 						</td>
 					</tr>
 				</tbody>
@@ -129,7 +129,7 @@
 			<div id="map" class=""></div>
 		</div>
 	</div>
-	<?php if (isset($results->gridReferenceWKT)) : ?>
+	@isset($results->gridReferenceWKT)
 		<script>
 			// Basic map code (in BasicMap.js) - not fitting to Shropshire
 			const map = initialiseBasicMap(fitToShropshire = false);
@@ -149,7 +149,7 @@
 			wktBoundary.addTo(map)
 			map.fitBounds(wktBoundary.getBounds(wktRecord).pad(0.5));
 		</script>
-	<?php endif ?>
+	@endisset
 
     @include('partials/download-link')
 
