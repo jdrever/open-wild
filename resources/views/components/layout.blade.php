ccbab8ee14317b69e3bb9eb3841ddd3f8b252086
James Drever <james@careful.digital>
2022-05-27 09:41:55 +0100

    simple sass setup

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta content="Promoting the enjoyment, understanding and conservation of the flora of Shropshire" name="description" />
 	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/' . strtolower(config('core.region')) .'-style.css') }}">
 	<!-- Custom styles for this template -->
 	<link href="/css/enhancements.css" rel="stylesheet">
 	<!-- A2HS -->
